6fe04d38cfc796ada0368e28e838dfcc7de58ebd
Michael Floßmann <michael.floszmann@gmail.com>
2017-05-30 17:14:12 +0200

    Finally properly done

--- sheet04/main.m
+++ sheet04/main.m
@@ -9,6 +9,9 @@ nv = 2;
 ne = 1;
 ni = 1;
 
+DELTA = 1e-3;
+x_test = [2,3];
+
 x = MX.sym('x',nv);
 
 f = (x(1)- 4)^2 + (x(2) - 4)^2;
@@ -20,51 +23,78 @@ h = x(1)^2 + x(2)^2 - 4;
 % Inequality contstraints
 H = Function('H',{x},{h});
 h_test = 9.0;
+if full(H(x_test)) - h_test > DELTA
+    error('Failure! H doesn`t fit!');
+end
 
 % Equality contstraints
 G = Function('G',{x},{g});
 G_test = -8.0907;
+if full(G(x_test)) - G_test > DELTA
+    error('Failure! G doesn`t fit!');
+end
 %
 % Objective
 F = Function('F',{x},{f});
 F_test = 5;
+if full(F(x_test)) - F_test > DELTA
+    error('Failure! F doesn`t fit!');
+end
 
 % Jacobian of g
 Jg = Function('Jg',{x},{jacobian(g,x)});
 Jg_test = [-0.4161,   -6.0000];
+if full(Jg(x_test)) - Jg_test > DELTA
+    error('Failure! Jg doesn`t fit!');
+end
 
 % Jacobian of h
 Jh = Function('Jh',{x},{jacobian(h,x)});
 Jh_test = [4,   6];
+if full(Jh(x_test)) - Jh_test > DELTA
+    error('Failure! Jh doesn`t fit!');
+end
 
 % Jacobian of f
 Jf = Function('Jf',{x},{jacobian(f,x)});
 Jf_test = [-4,   -2];
+if full(Jf(x_test)) - Jf_test > DELTA
+    error('Failure! Jf doesn`t fit!');
+end
 
 % Hessian of g
 Hg = Function('Hg',{x},{hessian(g,x)});
 Hg_test =    [  -0.9093,  0        ; ...
                 0,        -2.0000 ];
+if full(Hg(x_test)) - Hg_test > DELTA
+    error('Failure! Hg doesn`t fit!');
+end            
 % Hessian of h
 Hh = Function('Hh',{x},{hessian(h,x)});
 Hh_test =   [   2, 0; ...
                 0  2];
+if full(Hh(x_test)) - Hh_test > DELTA
+    error('Failure! Hh doesn`t fit!');
+end
 
 % Hessian of f
 % Hf = Function(...);
 Hf = Function('Hf',{x},{hessian(f,x)});
 Hf_test =   [   2, 0; ...
                 0  2];
+if full(Hf(x_test)) - Hf_test > DELTA
+    error('Failure! Hf doesn`t fit!');
+end
             
 % Interior point solver
 max_it = 100;
-xk = [-2;4];
+xk = [-2;-4];
 lk = 10*ones(ne,1);
 vk = 10*ones(ni,1);
 sk = 10*ones(ni,1);
 iter = zeros(nv + ne + ni + ni,max_it);
 iter(:,1) = [xk;lk;vk;sk];
-tau = 0;
+tau = 2;
 k_b = 1/3;
 th_1 = 1.0e-8;
 th_2 = 1.0e-8;
@@ -91,10 +121,9 @@ for i = 2:max_it
     
     rhs = - full([    (Jf_e + Jg_e*lk + Jh_e*vk)'; ...
                       g_e; ...
-                      h_e; ...
-                      vk*sk]);
-
-    
+                      h_e + sk; ...
+                      sk*vk - tau]);
+   
     % Termination condition
     if norm(rhs) < th_1
         if tau < th_2
@@ -107,7 +136,7 @@ for i = 2:max_it
      
     % YOUR CODE HERE:
     % Compute Newton step
-    sol = M\rhs;
+    sol = inv(M)*rhs;
     
     % line-serach
     max_ls = 100;
@@ -133,7 +162,7 @@ for i = 2:max_it
         
         % 0.7 seems like a good value. 0.9 doesn't converge and 0.5
         % terminates
-        alpha = alpha*0.7;
+        alpha = alpha*0.9;
         
         % Terminiation condition
         if norm(alpha*[ v_step;s_step]) < min_step
@@ -145,6 +174,7 @@ for i = 2:max_it
     lk  = lk + alpha*l_step;
     vk  = vk + alpha*v_step;
     sk  = sk + alpha*s_step;
+    
     
     % Print some results
     display(['Iteration: ',num2str(i)]);
